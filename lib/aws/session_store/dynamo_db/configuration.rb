00dda5fb3aa2636c904481838921c6f30312faee
Eng-Shien Wu <engshien.wu@playnext.com>
2015-05-29 19:06:24 -0700

    Remove support for getting endpoint from AWS_DYNAMODB_ENDPOINT

--- lib/aws/session_store/dynamo_db/configuration.rb
+++ lib/aws/session_store/dynamo_db/configuration.rb
@@ -204,8 +204,6 @@ module Aws::SessionStore::DynamoDB
       dynamo_db_client = @options[:dynamo_db_client]
       if dynamo_db_client.nil?
         client_opts = client_subset(@options)
-        # Support ENV to set endpoint to DynamoDB Local
-        client_opts[:endpoint] ||= ENV['AWS_DYNAMODB_ENDPOINT'] if ENV['AWS_DYNAMODB_ENDPOINT']
         dynamo_db_client = Aws::DynamoDB::Client.new(client_opts)
       end
 
