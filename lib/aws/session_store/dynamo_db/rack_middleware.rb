8cbf185ea94f42317986577d91af2030359273db
ryokdy <ryokdy@gmail.com>
2023-11-29 14:07:12 +0900

    Fix bug preventing saving of sessions with legacy format sid

--- lib/aws/session_store/dynamo_db/rack_middleware.rb
+++ lib/aws/session_store/dynamo_db/rack_middleware.rb
@@ -23,8 +23,7 @@ module Aws::SessionStore::DynamoDB
       if req.session.options[:skip]
         [generate_sid, {}]
       else
-        session = find_session_data(req, sid)
-        unless session
+        unless sid and session = @lock.get_session_data(req.env, get_session_id_with_fallback(sid))
           session = {}
           sid = generate_unique_sid(req.env, session)
         end
@@ -37,31 +36,30 @@ module Aws::SessionStore::DynamoDB
       OpenSSL::HMAC.hexdigest(OpenSSL::Digest::MD5.new, secret, sid).strip()
     end
 
-    # Get session data from DynamoDB.
-    def find_session_data(req, sid)
-      return nil unless sid
-      digest, ver_sid  = sid.public_id.split("--")
-      if ver_sid && @config.secret_key && digest == generate_hmac(ver_sid, @config.secret_key)
-        # Legacy session id format
-        @lock.get_session_data(req.env, sid.public_id)
-      else
-        @lock.get_session_data(req.env, sid.private_id)
-      end
-    end
-
     def generate_unique_sid(env, session)
       env['dynamo_db.new_session'] = 'true'
       generate_sid
     end
 
+    def get_session_id_with_fallback(sid)
+      return nil unless sid
+      digest, ver_sid = sid.public_id.split('--')
+      if ver_sid && @config.secret_key && digest == generate_hmac(ver_sid, @config.secret_key)
+        # Legacy session id format
+        sid.public_id
+      else
+        sid.private_id
+      end
+    end
+
     def write_session(req, sid, session, options)
-      sid = generate_sid if sid.nil? || !sid.respond_to?(:private_id)
-      @lock.set_session_data(req.env, sid.private_id, session, options)
+      sid = generate_sid if sid.nil?
+      @lock.set_session_data(req.env, get_session_id_with_fallback(sid), session, options)
       sid
     end
 
     def delete_session(req, sid, options)
-      @lock.delete_session(req.env, sid.private_id)
+      @lock.delete_session(req.env, get_session_id_with_fallback(sid))
       generate_sid unless options[:drop]
     end
   end
