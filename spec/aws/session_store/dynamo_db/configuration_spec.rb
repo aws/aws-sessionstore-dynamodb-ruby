692ccb61973c9349b0cfa583ac14e95805908647
Matt Muller <mamuller@amazon.com>
2024-10-19 16:18:07 -0400

    Improve configuration and tests

--- spec/aws/session_store/dynamo_db/configuration_spec.rb
+++ spec/aws/session_store/dynamo_db/configuration_spec.rb
@@ -1,81 +1,94 @@
 # frozen_string_literal: true
 
-# Copyright 2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License. A copy of
-# the License is located at
-#
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
-
 require 'spec_helper'
+require 'yaml'
 
 describe Aws::SessionStore::DynamoDB::Configuration do
   let(:defaults) do
+    Aws::SessionStore::DynamoDB::Configuration::MEMBERS.merge(
+      dynamo_db_client: kind_of(Aws::DynamoDB::Client),
+      error_handler: kind_of(Aws::SessionStore::DynamoDB::Errors::DefaultHandler)
+    )
+  end
+
+  let(:options) do
     {
-      table_name: 'sessions',
-      table_key: 'session_id',
-      consistent_read: true,
-      read_capacity: 10,
-      write_capacity: 5,
-      raise_errors: false
+      table_name: 'SessionTable',
+      table_key: 'SessionKey',
+      consistent_read: false,
+      read_capacity: 20,
+      write_capacity: 10,
+      raise_errors: true,
+      max_age: 7 * 60,
+      max_stale: 7,
+      enable_locking: true,
+      lock_expiry_time: 1000,
+      lock_retry_delay: 1000,
+      lock_max_wait_time: 2,
+      secret_key: 'SecretKey'
     }
   end
 
-  let(:expected_file_opts) do
-    {
-      consistent_read: true,
-      table_name: 'NewTable',
-      table_key: 'Somekey'
-    }
+  def setup_env
+    options.each do |k, v|
+      ENV["DYNAMO_DB_SESSION_#{k.to_s.upcase}"] = v.to_s
+    end
+  end
+
+  def teardown_env
+    options.each_key { |k| ENV.delete("DYNAMO_DB_SESSION_#{k.to_s.upcase}") }
   end
 
   let(:client) { Aws::DynamoDB::Client.new(stub_responses: true) }
-
-  let(:runtime_options) do
-    {
-      table_name: 'SessionTable',
-      table_key: 'session_id_stuff'
-    }
-  end
-
-  def expected_options(opts)
-    cfg = Aws::SessionStore::DynamoDB::Configuration.new(opts)
-    expected_opts = defaults.merge(expected_file_opts).merge(opts)
-    expect(cfg.to_hash).to include(expected_opts)
-  end
 
   before do
     allow(Aws::DynamoDB::Client).to receive(:new).and_return(client)
   end
 
-  context 'Configuration Tests' do
-    it 'configures option with out runtime,YAML or ENV options' do
+  it 'configures defaults without runtime, YAML or ENV options' do
+    cfg = Aws::SessionStore::DynamoDB::Configuration.new
+    expect(cfg.to_hash).to include(defaults)
+  end
+
+  it 'configures with ENV with precedence over defaults' do
+    setup_env
+    cfg = Aws::SessionStore::DynamoDB::Configuration.new
+    expect(cfg.to_hash).to include(options)
+    teardown_env
+  end
+
+  it 'configs with YAML with precedence over ENV' do
+    setup_env
+    Tempfile.create('dynamo_db_session_store.yml') do |f|
+      f << options.transform_keys(&:to_s).to_yaml
+      f.rewind
+      ENV['DYNAMO_DB_SESSION_CONFIG_FILE'] = f.path
       cfg = Aws::SessionStore::DynamoDB::Configuration.new
-      expect(cfg.to_hash).to include(defaults)
+      ENV.delete('DYNAMO_DB_SESSION_CONFIG_FILE')
+      expect(cfg.to_hash).to include(options)
     end
+    teardown_env
+  end
 
-    it 'configures accurate option hash with runtime options, no YAML or ENV' do
-      cfg = Aws::SessionStore::DynamoDB::Configuration.new(runtime_options)
-      expected_opts = defaults.merge(runtime_options)
-      expect(cfg.to_hash).to include(expected_opts)
+  it 'configures with runtime options with full precedence' do
+    setup_env
+    Tempfile.create('dynamo_db_session_store.yml') do |f|
+      f << { table_name: 'OldTable', table_key: 'OldKey' }.transform_keys(&:to_s).to_yaml
+      f.rewind
+      cfg = Aws::SessionStore::DynamoDB::Configuration.new(
+        options.merge(
+          config_file: f.path
+        )
+      )
+      expect(cfg.to_hash).to include(options)
     end
+    teardown_env
+  end
 
-    it 'merge YAML and runtime options giving runtime precendence' do
-      config_path = "#{File.dirname(__FILE__)}/app_config.yml"
-      runtime_opts = { config_file: config_path }.merge(runtime_options)
-      expected_options(runtime_opts)
-    end
-
-    it 'throws an exception when wrong path for file' do
-      config_path = 'Wrong path!'
-      runtime_opts = { config_file: config_path }.merge(runtime_options)
-      expect { Aws::SessionStore::DynamoDB::Configuration.new(runtime_opts) }.to raise_error(Errno::ENOENT)
-    end
+  it 'raises an exception when wrong path for file' do
+    config_path = 'Wrong path!'
+    runtime_opts = { config_file: config_path }.merge(options)
+    expect { Aws::SessionStore::DynamoDB::Configuration.new(runtime_opts) }
+      .to raise_error(Errno::ENOENT)
   end
 end
